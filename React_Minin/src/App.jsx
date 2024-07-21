74f7e6016e12f3966038fc2076582fce8270dd8f
Aleksei Melenchuk <melenchuk89@mail.ru>
2024-07-21 18:37:32 +0300

    Создаем React компонент

---
+++ React_Minin/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+      <div>
+        <Header/>
+        <Header/>
+        <main> 
+            <h1>Hello React!</h1>
+        </main>
+      </div>
+  )
+}
+
+export default App
