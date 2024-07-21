3f1e217538747821946df2cae440f649a2fdc3b6
Aleksei Melenchuk <melenchuk89@mail.ru>
2024-07-21 20:05:47 +0300

    Архитектура приложения, добавили файлы js css в отдельную папку

--- React_Minin/src/App.jsx
+++ React_Minin/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header"
 import {ways} from './data'
 import WayToTeach from "./components/WayToTeach"
-
+import Button from "./components/Button/Buttom"
 
 function App() {
 
@@ -16,13 +16,12 @@ function App() {
             <WayToTeach {...ways[1]}/>
             <WayToTeach {...ways[2]}/>
             <WayToTeach {...ways[3]}/>
-              {/* <li>
-                <p>
-                  <strong>Формат обучения.</strong> Пока другие предлогают 50 страниц текста и вебинары по 2 часа, мы даем вам тот же объем знаний в одном тщательно подготовленном видео за 15 минут.
-                </p>
-              </li> */}
             </ul>
            </section>
+          <section>
+            <h3>Чем мы отличаемся от других</h3>
+          <Button></Button>
+          </section>
         </main>
       </div>
   )
---
+++ React_Minin/src/components/Button/Buttom.jsx
@@ -0,0 +1,5 @@
+import "./Buttom.css"
+
+export default function Button(){
+    return <button className="button">Click</button>
+    }
